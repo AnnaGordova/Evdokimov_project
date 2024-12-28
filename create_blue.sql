131995d75022607522852c80a3b2b7815128797a
AnnaGordova <annagordova2@gmail.com>
2024-12-28 13:57:38 +0300

    Fixed bugs

--- create_blue.sql
+++ create_blue.sql
@@ -1,3 +1,8 @@
+CREATE TYPE enum_status_Post_catalogue AS ENUM ('м', 'ж', 'pending');
+CREATE TYPE enum_gender_Employee_catalogue AS ENUM('м', 'ж');
+CREATE TYPE enum_citizenship_Employee_catalogue AS ENUM('РФ', 'Иностранное');
+CREATE TYPE enum_is_confirmed_Weekly_report AS ENUM('подтвержден', 'не подтвержден');
+
 CREATE TABLE Passport_catalogue 
 (
 	id_passport SERIAL PRIMARY KEY,
@@ -7,6 +12,42 @@ CREATE TABLE Passport_catalogue
 	country VARCHAR(255),
 	valid_until DATE 
 );
+
+CREATE TABLE Post_catalogue
+(
+	id_post SERIAL PRIMARY KEY,
+	name VARCHAR(255),
+	description TEXT,
+	skill_level VARCHAR(50),
+	OKZ_code VARCHAR(20),
+	responsibilities TEXT,
+	status enum_status_Post_catalogue
+);
+
+CREATE TABLE Employee_catalogue
+(
+	id_employee SERIAL PRIMARY KEY,
+	surname VARCHAR(255),
+	name VARCHAR(255),
+	patronomyc VARCHAR(255),
+	birth DATE,
+	subdivision VARCHAR(255),
+	login VARCHAR(255),
+	password VARCHAR(255),
+	role VARCHAR(50),
+	work_number VARCHAR(20),
+	personal_number VARCHAR (20),
+	email VARCHAR(255),
+	gender enum_gender_Employee_catalogue,
+	INN VARCHAR (12),
+	SNILS VARCHAR(11),
+	date_employment DATE,
+	citizenship enum_citizenship_Employee_catalogue,
+	id_passport INTEGER REFERENCES Passport_catalogue (id_passport) ON DELETE CASCADE ON UPDATE CASCADE,
+	id_post INTEGER REFERENCES Post_catalogue (id_post) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
 
 CREATE TABLE Worklog 
 (
@@ -25,7 +66,7 @@ CREATE TABLE Weekly_report
 (
 	id_weekly_report SERIAL PRIMARY KEY,
 	date_create TIMESTAMP,
-	is_confirmed ENUM("подтвержден", "не подтвержден"),
+	is_confirmed enum_is_confirmed_Weekly_report,
 	note VARCHAR(255),
 	id_employee INTEGER REFERENCES Employee_catalogue (id_employee) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -41,16 +82,7 @@ CREATE TABLE Absence
 	id_employee INTEGER REFERENCES Employee_catalogue (id_employee) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Post_catalogue
-(
-	id_post SERIAL PRIMARY KEY,
-	name VARCHAR(255),
-	description TEXT,
-	skill_level VARCHAR(50),
-	OKZ_code VARCHAR(20),
-	responsibilities TEXT,
-	status ENUM("открыта", "закрыта")
-);
+
 
 CREATE TABLE Rate_catalogue
 (
@@ -60,7 +92,7 @@ CREATE TABLE Rate_catalogue
 	size NUMERIC(10, 2),
 	currency VARCHAR(3),
 	peridoicity VARCHAR(255),
-	id_post INTEGER REFERNCES Post_catalogue (id_post) ON DELETE CASCADE ON UPDATE CASCADE
+	id_post INTEGER REFERENCES Post_catalogue (id_post) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Worktime_catalogue
@@ -68,7 +100,7 @@ CREATE TABLE Worktime_catalogue
 	id_worktime SERIAL PRIMARY KEY,
 	hours_count NUMERIC(5, 2),
 	type_employment VARCHAR(255),
-	id_post INTEGER REFERNCES Post_catalogue (id_post) ON DELETE CASCADE ON UPDATE CASCADE
+	id_post INTEGER REFERENCES Post_catalogue (id_post) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Schedule_template
@@ -77,28 +109,6 @@ CREATE TABLE Schedule_template
 	day_week INTEGER,
 	time_start TIME,
 	time_end TIME,
-	id_post INTEGER REFERNCES Post_catalogue (id_post) ON DELETE CASCADE ON UPDATE CASCADE
+	id_post INTEGER REFERENCES Post_catalogue (id_post) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Employee_catalogue
-(
-	id_employee SERIAL PRIMARY KEY,
-	surname VARCHAR(255),
-	name VARCHAR(255),
-	patronomyc VARCHAR(255),
-	birth DATE,
-	subdivision VARCHAR(255),
-	login VARCHAR(255),
-	password VARCHAR(255),
-	role VARCHAR(50),
-	work_number VARCHAR(20),
-	personal_number VARCHAR (20),
-	email VARCHAR(255),
-	gender ENUM("м", "ж"),
-	INN VARCHAR (12),
-	SNILS VARCHAR(11),
-	date_employment DATE,
-	citizenship ENUM("РФ", "Иностранное"),
-	id_passport INTEGER REFERNCES Passport_catalogue (id_passport) ON DELETE CASCADE ON UPDATE CASCADE,
-	id_post INTEGER REFERNCES Post_catalogue (id_post) ON DELETE CASCADE ON UPDATE CASCADE
-);
